2c744db7c54eeb8ad47a375a0c475e8cf29e86d1
Kavan <kavan@Kavan.fritz.box>
2023-03-25 13:20:14 +0100

    sveltekit boilerplate with jsonserver and tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="main-container">
+<h1 class="text-3xl text-red-600">
+    Welcome to the new boilerplate
+</h1>
+</main>
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.300);
+  }
+</style>
